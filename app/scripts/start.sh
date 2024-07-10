f1c02cdd634c93842b56aba31e9cfb446d0dee6b
Ashmiza Shah <ashshanayaofficial786143@gmail.com>
2024-07-10 12:46:47 +0530

    multi-stages

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -23,3 +23,7 @@ export NODE_ENV
 
 # Start the Node.js application with the specified config file
 nohup /root/.nvm/versions/node/v20.14.0/bin/node /opt/apps/index.js --config $CONFIG_FILE > /opt/apps/start.log 2>&1 &
+
+
+
+
