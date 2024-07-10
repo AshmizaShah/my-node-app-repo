f0a2b8539eb53bb7c88181481432f9084ceae625
Ashmiza Shah <ashshanayaofficial786143@gmail.com>
2024-07-11 00:12:21 +0530

    ashmiza main

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -3,12 +3,12 @@
 # Stop any running instances of node
 killall -9 node || true
 
-# Set the environment and config file based on the ENV variable
-if [ "$ENV" == "production" ]; then
+# Determine the environment based on DEPLOYMENT_GROUP_NAME
+if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]; then
   NODE_ENV=production
   CONFIG_FILE=config.prod.json
   echo "Starting application in production environment..."
-elif [ "$ENV" == "development" ]; then
+elif [ "$DEPLOYMENT_GROUP_NAME" == "development" ]; then
   NODE_ENV=development
   CONFIG_FILE=config.dev.json
   echo "Starting application in development environment..."
@@ -23,7 +23,3 @@ export NODE_ENV
 
 # Start the Node.js application with the specified config file
 nohup /root/.nvm/versions/node/v20.14.0/bin/node /opt/apps/index.js --config $CONFIG_FILE > /opt/apps/start.log 2>&1 &
-
-
-
-
