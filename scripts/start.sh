f85b6c93ebb8a34603d77fad76f1cf52fc97e36e
AshmizaShah <138257656+AshmizaShah@users.noreply.github.com>
2024-07-11 14:15:06 +0530

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+# Stop any running instances of node
+killall -9 node || true
+
+# Determine the environment based on DEPLOYMENT_GROUP_NAME
+if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]; then
+  NODE_ENV=production
+  CONFIG_FILE=config.prod.json
+  echo "Starting application in production environment..."
+elif [ "$DEPLOYMENT_GROUP_NAME" == "development" ]; then
+  NODE_ENV=development
+  CONFIG_FILE=config.dev.json
+  echo "Starting application in development environment..."
+else
+  NODE_ENV=default
+  CONFIG_FILE=config.json
+  echo "Starting application in default environment..."
+fi
+
+# Export the environment variable
+export NODE_ENV
+
+# Start the Node.js application with the specified config file
+nohup /root/.nvm/versions/node/v20.14.0/bin/node /opt/apps/index.js --config $CONFIG_FILE > /opt/apps/start.log 2>&1 &
